df295ca951631a08441b6176776fffdf81a60b68
pradeepben3 <97969637+pradeepben3@users.noreply.github.com>
2022-12-18 18:42:58 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -8,3 +8,12 @@ int our_function1(int num_numbers, int *numbers) {
     }
     return sum;
 }
+
+int our_function(int num_numbers, int *numbers) {
+    int i;
+    int sum = 0;
+    for (i = 0; i < num_numbers; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
